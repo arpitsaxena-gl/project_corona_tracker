da35fc0ca4d405eb20f38a1ffd1fc8deec70a2a2
arpitsaxena-gl <arpit.saxena2@globallogic.com>
2026-08-17 17:16:57 +0530

    feat: add src/components/CountryPicker/CountryPicker.jsx [Redmine #8]

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,29 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
-
 import { fetchCountries } from '../../api';
-
 import styles from './CountryPicker.module.css';
 
-const Countries = ({ handleCountryChange }) => {
+const CountryPicker = ({ handleCountryChange }) => {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    const fetchAPI = async () => {
-      setCountries(await fetchCountries());
-    };
-
-    fetchAPI();
+    fetchCountries()
+      .then((names) => setCountries(names))
+      .catch((err) => console.error('Failed to load countries:', err.message));
   }, []);
 
   return (
     <FormControl className={styles.formControl}>
-      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
-        <option value="">United States</option>
-        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      <NativeSelect defaultValue="" onChange={handleCountryChange}>
+        <option value="">Global</option>
+        {countries.map((country) => (
+          <option key={country} value={country}>{country}</option>
+        ))}
       </NativeSelect>
     </FormControl>
   );
 };
 
-export default Countries;
+export default CountryPicker;
